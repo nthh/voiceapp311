7548b9970f516423cdf979eedf1756a7f51df043
dstair <dstair@cazena.com>
2018-05-23 23:29:43 -0400

    food truck intent pulls data from arcgis

--- mycity/mycity/deploy_tools/install_requirements.sh
+++ mycity/mycity/deploy_tools/install_requirements.sh
@@ -6,8 +6,8 @@ REQUIREMENTS_FILE="./requirements.txt"
 REQUIREMENTS_NO_DEPS_FILE="./requirements_no_deps.txt"
 TEMP_DIR_PATH="../../temp"
 
-pip install -r $REQUIREMENTS_FILE -t $TEMP_DIR_PATH
+pip3 install -r $REQUIREMENTS_FILE -t $TEMP_DIR_PATH
 
-pip install --no-deps -r $REQUIREMENTS_NO_DEPS_FILE -t $TEMP_DIR_PATH
+pip3 install --no-deps -r $REQUIREMENTS_NO_DEPS_FILE -t $TEMP_DIR_PATH
 
 
